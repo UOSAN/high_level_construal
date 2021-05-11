7453abce5a073800aa4c35c53bd7bc187665c3ee
Paul Novak <pnovak@alumni.caltech.edu>
2021-05-11 13:41:57 -0700

    Don't store data from some components, workaround issue for others
    For some components, don't store the start and stop time, because it
    is not needed. For other components, explicitly store the stop time
    for the last component in a routine to work around issue
    https://github.com/psychopy/psychopy/issues/3672

--- construal_level_task.py
+++ construal_level_task.py
@@ -72,7 +72,7 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
     # Store info about the experiment session
     psychopyVersion = '2021.1.2'
     expName = 'construal_level'  # from the Builder filename that created this script
-    expInfo = expInfo = {'participant': participant_id, 'session': session, 'run_number': run_number}
+    expInfo = {'participant': participant_id, 'session': session, 'run_number': run_number}
     expInfo['date'] = data.getDateStr()  # add a simple timestamp
     expInfo['expName'] = expName
     expInfo['psychopyVersion'] = psychopyVersion
@@ -142,32 +142,29 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
     # Initialize components for Routine "block_setup"
     block_setupClock = core.Clock()
 
-    # Initialize components for Routine "cue"
-    cueClock = core.Clock()
+    # Initialize components for Routine "action"
+    actionClock = core.Clock()
     cue_str = visual.TextStim(win=win, name='cue_str',
                               text='',
                               font='Helvetica',
-                              pos=(0, 0), height=0.05, wrapWidth=None, ori=0,
-                              color='white', colorSpace='rgb', opacity=1,
+                              pos=(0, 0), height=0.05, wrapWidth=None, ori=0.0,
+                              color='white', colorSpace='rgb', opacity=None,
                               languageStyle='LTR',
-                              depth=0.0);
-
-    # Initialize components for Routine "action"
-    actionClock = core.Clock()
+                              depth=-1.0);
     action_text = visual.TextStim(win=win, name='action_text',
                                   text='',
                                   font='Helvetica',
                                   pos=(0, 0.25), height=0.05, wrapWidth=None, ori=0,
                                   color='white', colorSpace='rgb', opacity=1,
                                   languageStyle='LTR',
-                                  depth=-1.0);
+                                  depth=-2.0);
     attention_check = visual.TextStim(win=win, name='attention_check',
                                       text='',
                                       font='Helvetica',
                                       pos=(0, 0), height=0.05, wrapWidth=None, ori=0,
                                       color='white', colorSpace='rgb', opacity=1,
                                       languageStyle='LTR',
-                                      depth=-2.0);
+                                      depth=-3.0);
     attention_check_rating = visual.Slider(win=win, name='attention_check_rating',
                                            size=(1.0, 0.025), pos=(0, -0.3), units=None,
                                            labels=['not at all\noften', 'somewhat\noften', 'moderately\noften',
@@ -176,7 +173,7 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
                                            style='rating', styleTweaks=('triangleMarker',), opacity=1,
                                            color='LightGray', fillColor='Red', borderColor='White', colorSpace='rgb',
                                            font='Helvetica', labelHeight=0.03,
-                                           flip=False, depth=-3, readOnly=False)
+                                           flip=False, depth=-4, readOnly=False)
     attention_check_keyboard = keyboard.Keyboard()
 
     # Initialize components for Routine "scenario_cue"
@@ -197,7 +194,7 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
                                   pos=(0, 0), height=0.05, wrapWidth=None, ori=0,
                                   color='white', colorSpace='rgb', opacity=1,
                                   languageStyle='LTR',
-                                  depth=0.0);
+                                  depth=-1.0);
     block1_sound = sound.Sound('A', secs=-1, stereo=True, hamming=True,
                                name='block1_sound')
     block1_sound.setVolume(1)
@@ -210,7 +207,7 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
                                   pos=(0, 0), height=0.05, wrapWidth=None, ori=0,
                                   color='white', colorSpace='rgb', opacity=1,
                                   languageStyle='LTR',
-                                  depth=0.0);
+                                  depth=-1.0);
     block2_sound = sound.Sound('A', secs=-1, stereo=True, hamming=True,
                                name='block2_sound')
     block2_sound.setVolume(1)
@@ -223,7 +220,7 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
                                   pos=(0, 0), height=0.05, wrapWidth=None, ori=0,
                                   color='white', colorSpace='rgb', opacity=1,
                                   languageStyle='LTR',
-                                  depth=0.0);
+                                  depth=-1.0);
     block3_sound = sound.Sound('A', secs=-1, stereo=True, hamming=True,
                                name='block3_sound')
     block3_sound.setVolume(1)
@@ -429,8 +426,6 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
     for thisComponent in instructionsComponents:
         if hasattr(thisComponent, "setAutoDraw"):
             thisComponent.setAutoDraw(False)
-    thisExp.addData('title_text.started', title_text.tStartRefresh)
-    thisExp.addData('title_text.stopped', title_text.tStopRefresh)
     # the Routine "instructions" was not non-slip safe, so reset the non-slip timer
     routineTimer.reset()
 
@@ -516,86 +511,21 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
 
             for thisAction_trial in action_trials:
                 currentLoop = action_trials
-
-                # ------Prepare to start Routine "cue"-------
-                continueRoutine = True
-                routineTimer.add(4.000000)
-                # update component parameters for each repeat
-                cue_str.setText("Please imagine the next events occurring " + thisBlock['cue_text'])
-                # keep track of which components have finished
-                cueComponents = [cue_str]
-                for thisComponent in cueComponents:
-                    thisComponent.tStart = None
-                    thisComponent.tStop = None
-                    thisComponent.tStartRefresh = None
-                    thisComponent.tStopRefresh = None
-                    if hasattr(thisComponent, 'status'):
-                        thisComponent.status = NOT_STARTED
-                # reset timers
-                t = 0
-                _timeToFirstFrame = win.getFutureFlipTime(clock="now")
-                cueClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
-                frameN = -1
-
-                # -------Run Routine "cue"-------
-                while continueRoutine and routineTimer.getTime() > 0:
-                    # get current time
-                    t = cueClock.getTime()
-                    tThisFlip = win.getFutureFlipTime(clock=cueClock)
-                    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
-                    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
-                    # update/draw components on each frame
-
-                    # *cue_str* updates
-                    if cue_str.status == NOT_STARTED and tThisFlip >= 0.0 - frameTolerance:
-                        # keep track of start time/frame for later
-                        cue_str.frameNStart = frameN  # exact frame index
-                        cue_str.tStart = t  # local t and not account for scr refresh
-                        cue_str.tStartRefresh = tThisFlipGlobal  # on global time
-                        win.timeOnFlip(cue_str, 'tStartRefresh')  # time at next scr refresh
-                        cue_str.setAutoDraw(True)
-                    if cue_str.status == STARTED:
-                        # is it time to stop? (based on global clock, using actual start)
-                        if tThisFlipGlobal > cue_str.tStartRefresh + 4.0 - frameTolerance:
-                            # keep track of stop time/frame for later
-                            cue_str.tStop = t  # not accounting for scr refresh
-                            cue_str.frameNStop = frameN  # exact frame index
-                            win.timeOnFlip(cue_str, 'tStopRefresh')  # time at next scr refresh
-                            cue_str.setAutoDraw(False)
-
-                    # check for quit (typically the Esc key)
-                    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
-                        core.quit()
-
-                    # check if all components have finished
-                    if not continueRoutine:  # a component has requested a forced-end of Routine
-                        break
-                    continueRoutine = False  # will revert to True if at least one component still running
-                    for thisComponent in cueComponents:
-                        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
-                            continueRoutine = True
-                            break  # at least one component has not yet finished
-
-                    # refresh the screen
-                    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
-                        win.flip()
-
-                # -------Ending Routine "cue"-------
-                for thisComponent in cueComponents:
-                    if hasattr(thisComponent, "setAutoDraw"):
-                        thisComponent.setAutoDraw(False)
-                action_trials.addData('cue_str.started', cue_str.tStartRefresh)
-                action_trials.addData('cue_str.stopped', cue_str.tStopRefresh)
+                # abbreviate parameter names if possible (e.g. rgb = thisAction_trial.rgb)
+                if thisAction_trial != None:
+                    for paramName in thisAction_trial:
+                        exec('{} = thisAction_trial[paramName]'.format(paramName))
 
                 # ------Prepare to start Routine "action"-------
                 continueRoutine = True
-                routineTimer.add(5.000000)
+                routineTimer.add(9.000000)
                 # update component parameters for each repeat
                 if thisBlock['condition_type'] == 'present':
                     attention_check_text = 'How often do you engage in this action\nRIGHT NOW?'
                 else:
                     attention_check_text = 'How often would you engage in this action\nFIVE YEARS FROM NOW?'
 
+                cue_str.setText("Please imagine the next events occurring " + thisBlock['cue_text'])
                 action_text.setText(thisAction_trial['action'])
                 attention_check.setText(attention_check_text)
                 attention_check_rating.reset()
@@ -603,7 +533,8 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
                 attention_check_keyboard.rt = []
                 _attention_check_keyboard_allKeys = []
                 # keep track of which components have finished
-                actionComponents = [action_text, attention_check, attention_check_rating, attention_check_keyboard]
+                actionComponents = [cue_str, action_text, attention_check, attention_check_rating,
+                                    attention_check_keyboard]
                 for thisComponent in actionComponents:
                     thisComponent.tStart = None
                     thisComponent.tStop = None
@@ -632,8 +563,25 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
                     # confirm rating by setting to current markerPos
                     attention_check_rating.rating = r
 
+                    # *cue_str* updates
+                    if cue_str.status == NOT_STARTED and tThisFlip >= 0.0 - frameTolerance:
+                        # keep track of start time/frame for later
+                        cue_str.frameNStart = frameN  # exact frame index
+                        cue_str.tStart = t  # local t and not account for scr refresh
+                        cue_str.tStartRefresh = tThisFlipGlobal  # on global time
+                        win.timeOnFlip(cue_str, 'tStartRefresh')  # time at next scr refresh
+                        cue_str.setAutoDraw(True)
+                    if cue_str.status == STARTED:
+                        # is it time to stop? (based on global clock, using actual start)
+                        if tThisFlipGlobal > cue_str.tStartRefresh + 4.0 - frameTolerance:
+                            # keep track of stop time/frame for later
+                            cue_str.tStop = t  # not accounting for scr refresh
+                            cue_str.frameNStop = frameN  # exact frame index
+                            win.timeOnFlip(cue_str, 'tStopRefresh')  # time at next scr refresh
+                            cue_str.setAutoDraw(False)
+
                     # *action_text* updates
-                    if action_text.status == NOT_STARTED and tThisFlip >= 0.0 - frameTolerance:
+                    if action_text.status == NOT_STARTED and tThisFlip >= 4 - frameTolerance:
                         # keep track of start time/frame for later
                         action_text.frameNStart = frameN  # exact frame index
                         action_text.tStart = t  # local t and not account for scr refresh
@@ -650,7 +598,7 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
                             action_text.setAutoDraw(False)
 
                     # *attention_check* updates
-                    if attention_check.status == NOT_STARTED and tThisFlip >= 0.0 - frameTolerance:
+                    if attention_check.status == NOT_STARTED and tThisFlip >= 4 - frameTolerance:
                         # keep track of start time/frame for later
                         attention_check.frameNStart = frameN  # exact frame index
                         attention_check.tStart = t  # local t and not account for scr refresh
@@ -667,7 +615,7 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
                             attention_check.setAutoDraw(False)
 
                     # *attention_check_rating* updates
-                    if attention_check_rating.status == NOT_STARTED and tThisFlip >= 0.0 - frameTolerance:
+                    if attention_check_rating.status == NOT_STARTED and tThisFlip >= 4 - frameTolerance:
                         # keep track of start time/frame for later
                         attention_check_rating.frameNStart = frameN  # exact frame index
                         attention_check_rating.tStart = t  # local t and not account for scr refresh
@@ -685,7 +633,7 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
 
                     # *attention_check_keyboard* updates
                     waitOnFlip = False
-                    if attention_check_keyboard.status == NOT_STARTED and tThisFlip >= 0.0 - frameTolerance:
+                    if attention_check_keyboard.status == NOT_STARTED and tThisFlip >= 4 - frameTolerance:
                         # keep track of start time/frame for later
                         attention_check_keyboard.frameNStart = frameN  # exact frame index
                         attention_check_keyboard.tStart = t  # local t and not account for scr refresh
@@ -735,8 +683,11 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
                 for thisComponent in actionComponents:
                     if hasattr(thisComponent, "setAutoDraw"):
                         thisComponent.setAutoDraw(False)
+                attention_check_rating.tStopRefresh = tThisFlipGlobal
+                attention_check_keyboard.tStopRefresh = tThisFlipGlobal
+                action_trials.addData('cue_str.started', cue_str.tStartRefresh)
+                action_trials.addData('cue_str.stopped', cue_str.tStopRefresh)
                 action_trials.addData('attention_check_rating.response', attention_check_rating.getRating())
-                action_trials.addData('attention_check_rating.rt', attention_check_rating.getRT())
                 action_trials.addData('attention_check_rating.started', attention_check_rating.tStartRefresh)
                 action_trials.addData('attention_check_rating.stopped', attention_check_rating.tStopRefresh)
                 # check responses
@@ -894,6 +845,8 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
             for thisComponent in scenario1Components:
                 if hasattr(thisComponent, "setAutoDraw"):
                     thisComponent.setAutoDraw(False)
+            block1_text.tStopRefresh = tThisFlipGlobal
+            block1_sound.tStopRefresh = tThisFlipGlobal
             block1_sound.stop()  # ensure sound has stopped at end of routine
             scenario_trials.addData('block1_sound.started', block1_sound.tStartRefresh)
             scenario_trials.addData('block1_sound.stopped', block1_sound.tStopRefresh)
@@ -974,7 +927,11 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
             for thisComponent in scenario2Components:
                 if hasattr(thisComponent, "setAutoDraw"):
                     thisComponent.setAutoDraw(False)
+            block2_text.tStopRefresh = tThisFlipGlobal
+            block2_sound.tStopRefresh = tThisFlipGlobal
             block2_sound.stop()  # ensure sound has stopped at end of routine
+            scenario_trials.addData('block2_sound.started', block2_sound.tStartRefresh)
+            scenario_trials.addData('block2_sound.stopped', block2_sound.tStopRefresh)
             # the Routine "scenario2" was not non-slip safe, so reset the non-slip timer
             routineTimer.reset()
 
@@ -1052,8 +1009,8 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
             for thisComponent in scenario3Components:
                 if hasattr(thisComponent, "setAutoDraw"):
                     thisComponent.setAutoDraw(False)
-            scenario_trials.addData('block3_text.started', block3_text.tStartRefresh)
-            scenario_trials.addData('block3_text.stopped', block3_text.tStopRefresh)
+            block3_text.tStopRefresh = tThisFlipGlobal
+            block3_sound.tStopRefresh = tThisFlipGlobal
             block3_sound.stop()  # ensure sound has stopped at end of routine
             scenario_trials.addData('block3_sound.started', block3_sound.tStartRefresh)
             scenario_trials.addData('block3_sound.stopped', block3_sound.tStopRefresh)
@@ -1187,8 +1144,9 @@ def clt(participant_id: str, session: str, run_number: str, is_first: bool):
             for thisComponent in quitting_intentionComponents:
                 if hasattr(thisComponent, "setAutoDraw"):
                     thisComponent.setAutoDraw(False)
+            quitting_intention_rating.tStopRefresh = tThisFlipGlobal
+            quitting_intention_keyboard.tStopRefresh = tThisFlipGlobal
             scenario_trials.addData('quitting_intention_rating.response', quitting_intention_rating.getRating())
-            scenario_trials.addData('quitting_intention_rating.rt', quitting_intention_rating.getRT())
             scenario_trials.addData('quitting_intention_rating.started', quitting_intention_rating.tStartRefresh)
             scenario_trials.addData('quitting_intention_rating.stopped', quitting_intention_rating.tStopRefresh)
             # check responses
